d7b956d2d181c3d46cda930fc46ef7f6f5039e66
Benjamin Jørgensen <benjamin@bnjamin.com>
2011-05-15 02:15:10 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tomatoid::Application.config.secret_token = 'c4e0bf1ddbd500ff651f7cdc26cc50db6553cfe676d0f2c67e9408962b3edd2bdd3f6cb27d44cd9595de8f6f092f8982c5d2315ec3227e5e28574e07f44b7207'
